248ce766d5e65667e1b9610922f94839d8724bf7
GatoJustiniano <andres95liverpool@gmail.com>
2024-07-20 00:04:28 -0400

    session out

--- lib/presentation/screen/counter_screen.dart
+++ lib/presentation/screen/counter_screen.dart
@@ -10,33 +10,72 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
-
   int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Vista contador'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('$clickCounter', style: const TextStyle(fontSize: 100),),
-            Text('Click${ clickCounter == 1 ? '' : 's'}'),
+        appBar: AppBar(
+          title: const Text('Vista contador'),
+          actions: [
+            CustomButton(
+              icon: Icons.refresh,
+              onPressed: () {
+                clickCounter = 0;
+                setState(() {});
+              },
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          clickCounter++;
-          setState(() {
-            
-          });
-        },
-        child: const Icon(Icons.plus_one),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$clickCounter',
+                style: const TextStyle(fontSize: 100),
+              ),
+              Text('Click${clickCounter == 1 ? '' : 's'}'),
+            ],
+          ),
         ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            CustomButton(
+              icon: Icons.plus_one,
+              onPressed: () {
+                clickCounter++;
+                setState(() {});
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomButton(
+              icon: Icons.exposure_minus_1_rounded,
+              onPressed: () {
+                if (clickCounter == 0) return;
+                clickCounter--;
+                setState(() {});
+              },
+            ),
+          ],
+        ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+
+  const CustomButton({super.key, required this.icon, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onPressed,
+      child: Icon(icon),
     );
   }
 }
