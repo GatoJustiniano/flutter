f993de5d567f9ec5d9b5ebff3983e912821ee8c7
GatoJustiniano <andres95liverpool@gmail.com>
2024-07-19 23:26:37 -0400

    seccion one

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_la_garrita/presentation/screen/counter_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'sample_feature/sample_item_details_view.dart';
@@ -34,7 +35,9 @@ class MyApp extends StatelessWidget {
           ],
           onGenerateTitle: (BuildContext context) =>
               AppLocalizations.of(context)!.appTitle,
-          theme: ThemeData(),
+          theme: ThemeData(
+            useMaterial3: true
+          ),
           darkTheme: ThemeData.dark(),
           themeMode: settingsController.themeMode,
           debugShowCheckedModeBanner: false,
@@ -48,6 +51,8 @@ class MyApp extends StatelessWidget {
                     return SettingsView(controller: settingsController);
                   case SampleItemDetailsView.routeName:
                     return const SampleItemDetailsView();
+                  case CounterScreen.routeName:
+                    return const CounterScreen();
                   case SampleItemListView.routeName:
                   default:
                     return const SampleItemListView();
--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_la_garrita/presentation/screen/counter_screen.dart';
 
 import '../settings/settings_view.dart';
 import 'sample_item.dart';
@@ -19,30 +20,23 @@ class SampleItemListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sample Items'),
+        title: const Text('Listado de items'),
         actions: [
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {
-              // Navigate to the settings page. If the user leaves and returns
-              // to the app after it has been killed while running in the
-              // background, the navigation stack is restored.
               Navigator.restorablePushNamed(context, SettingsView.routeName);
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.countertops_sharp),
+            onPressed: () {
+              Navigator.restorablePushNamed(context, CounterScreen.routeName);
             },
           ),
         ],
       ),
-
-      // To work with lists that may contain a large number of items, it’s best
-      // to use the ListView.builder constructor.
-      //
-      // In contrast to the default ListView constructor, which requires
-      // building all Widgets up front, the ListView.builder constructor lazily
-      // builds Widgets as they’re scrolled into view.
       body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
         restorationId: 'sampleItemListView',
         itemCount: items.length,
         itemBuilder: (BuildContext context, int index) {
@@ -51,13 +45,9 @@ class SampleItemListView extends StatelessWidget {
           return ListTile(
             title: Text('Elemento nro. ${item.id}'),
             leading: const CircleAvatar(
-              // Display the Flutter Logo image asset.
               foregroundImage: AssetImage('assets/images/flutter_logo.png'),
             ),
             onTap: () {
-              // Navigate to the details page. If the user leaves and returns to
-              // the app after it has been killed while running in the
-              // background, the navigation stack is restored.
               Navigator.restorablePushNamed(
                 context,
                 SampleItemDetailsView.routeName,
@@ -65,6 +55,7 @@ class SampleItemListView extends StatelessWidget {
             }
           );
         },
+        
       ),
     );
   }
