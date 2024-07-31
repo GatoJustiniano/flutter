cfba757bcac461a026806242ddbf98c9901c2340
GatoJustiniano <andres95liverpool@gmail.com>
2024-07-30 23:37:18 -0400

    card

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -4,7 +4,7 @@ import 'package:go_router/go_router.dart';
 
 /// The route configuration.
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/buttons',
+  initialLocation: '/cards',
   routes: [
     GoRoute(
       name: HomeScreen.routeName,
--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,10 +1,16 @@
-
 import 'package:flutter/material.dart';
 
+const listCard = <Map<String, dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevación 0'},
+  {'elevation': 1.0, 'label': 'Elevación 1'},
+  {'elevation': 2.0, 'label': 'Elevación 2'},
+  {'elevation': 3.0, 'label': 'Elevación 3'},
+  {'elevation': 4.0, 'label': 'Elevación 4'},
+  {'elevation': 5.0, 'label': 'Elevación 5'},
+];
+
 class CardsScreen extends StatelessWidget {
-
   static const routeName = 'cards_screen';
-
   const CardsScreen({super.key});
 
   @override
@@ -13,7 +19,54 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
-      body: Text('cuerpo de la tarjeta'),
+      body: _CardView(),
+    );
+  }
+}
+
+class _CardView extends StatelessWidget {
+  const _CardView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...listCard.map((card) =>
+              _CardType1(label: card['label'], elevation: card['elevation'])),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.battery_unknown),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
