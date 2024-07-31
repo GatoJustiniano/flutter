5df933cb697401724cd9a28cec239f856b225f38
GatoJustiniano <andres95liverpool@gmail.com>
2024-07-30 23:45:49 -0400

    border outline

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -34,12 +34,15 @@ class _CardView extends StatelessWidget {
         children: [
           ...listCard.map((card) =>
               _CardType1(label: card['label'], elevation: card['elevation'])),
+          ...listCard.map((card) =>
+              _CardType2(label: card['label'], elevation: card['elevation'])),
         ],
       ),
     );
   }
 }
 
+// card normal
 class _CardType1 extends StatelessWidget {
   final String label;
   final double elevation;
@@ -63,6 +66,45 @@ class _CardType1 extends StatelessWidget {
             Align(
               alignment: Alignment.bottomLeft,
               child: Text(label),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// card border
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(
+          color: colors.outline,
+        )
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.battery_unknown),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text('$label - outline'),
             ),
           ],
         ),
