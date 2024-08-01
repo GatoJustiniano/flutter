099bb35a7023c67a71a175cae25af3510630c11b
GatoJustiniano <andres95liverpool@gmail.com>
2024-07-31 22:53:52 -0400

    dialog custom

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter_la_garrita/config/router/app_router.dart';
 import 'package:flutter_la_garrita/config/theme/app_theme.dart';
 
 void main() => runApp(MaterialApp.router(
+      title: 'Flutter La Garrita',
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 7).getTheme(),
       routerConfig: appRouter,
     ));
-
