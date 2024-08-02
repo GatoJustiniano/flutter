1828d234441f0520b7e9e34f0cf4751b500dbc11
GatoJustiniano <andres95liverpool@gmail.com>
2024-08-02 02:58:52 -0400

    widget custom-button

--- lib/presentation/screens/others/counter_riverpood_screen.dart
+++ lib/presentation/screens/others/counter_riverpood_screen.dart
@@ -1,46 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_la_garrita/presentation/providers/counter_provider.dart';
+import 'package:flutter_la_garrita/presentation/widgets/custom_buttom.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
-
 class CounterRiverpoodScreen extends ConsumerWidget {
-
   static const routeName = 'counter_riverpood_screen';
-
 
   const CounterRiverpoodScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
-    final int clickCounter = ref.watch( counterProvider );
+    final int clickCounter = ref.watch(counterProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Counter Screen'),
+        title: const Text('Counter Riverpood'),
         actions: [
           IconButton(
-            icon: Icon( Icons.dark_mode_outlined ),
-            onPressed: () {
-              
-            }, 
+            icon: Icon(Icons.dark_mode_outlined),
+            onPressed: () {},
           )
         ],
       ),
-
       body: Center(
-        child: Text('Valor: $clickCounter', style: Theme.of(context).textTheme.titleLarge ),
+        child: Text(
+          'Valor: $clickCounter',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
       ),
-
-      floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.add ),
-        onPressed: (){
-          ref.read( counterProvider.notifier ).state++;
-
-        },
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomButton(
+            icon: Icons.plus_one,
+            onPressed: () {
+              ref.read(counterProvider.notifier).state++;
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomButton(
+            icon: Icons.exposure_minus_1_rounded,
+            onPressed: () {
+              if (clickCounter == 0) return;
+              ref.read(counterProvider.notifier).state--;
+            },
+          ),
+        ],
       ),
-      
     );
   }
 }
--- lib/presentation/screens/others/counter_screen.dart
+++ lib/presentation/screens/others/counter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_la_garrita/presentation/widgets/custom_buttom.dart';
 
 class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
@@ -15,67 +16,55 @@ class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Vista contador'),
-          actions: [
-            CustomButton(
-              icon: Icons.refresh,
-              onPressed: () {
-                clickCounter = 0;
-                setState(() {});
-              },
-            ),
-          ],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '$clickCounter',
-                style: const TextStyle(fontSize: 100),
-              ),
-              Text('Click${clickCounter == 1 ? '' : 's'}'),
-            ],
+      appBar: AppBar(
+        title: const Text('Vista contador'),
+        actions: [
+          CustomButton(
+            icon: Icons.refresh,
+            onPressed: () {
+              clickCounter = 0;
+              setState(() {});
+            },
           ),
-        ),
-        floatingActionButton: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CustomButton(
-              icon: Icons.plus_one,
-              onPressed: () {
-                clickCounter++;
-                setState(() {});
-              },
+            Text(
+              '$clickCounter',
+              style: const TextStyle(fontSize: 100),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomButton(
-              icon: Icons.exposure_minus_1_rounded,
-              onPressed: () {
-                if (clickCounter == 0) return;
-                clickCounter--;
-                setState(() {});
-              },
-            ),
+            Text('Click${clickCounter == 1 ? '' : 's'}'),
           ],
-        ));
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final IconData icon;
-  final VoidCallback? onPressed;
-
-  const CustomButton({super.key, required this.icon, this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: onPressed,
-      child: Icon(icon),
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomButton(
+            icon: Icons.plus_one,
+            onPressed: () {
+              clickCounter++;
+              setState(() {});
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          CustomButton(
+            icon: Icons.exposure_minus_1_rounded,
+            onPressed: () {
+              if (clickCounter == 0) return;
+              clickCounter--;
+              setState(() {});
+            },
+          ),
+        ],
+      ),
     );
   }
 }
+
+
