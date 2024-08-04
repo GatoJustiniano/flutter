82f70e583f8451ff1044875d09e3bf275ac94718
GatoJustiniano <andres95liverpool@gmail.com>
2024-08-03 23:01:52 -0400

    mejoras

--- lib/presentation/screens/settings/settings_screen.dart
+++ lib/presentation/screens/settings/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_la_garrita/config/menu/menu_items.dart';
+import 'package:flutter_la_garrita/presentation/providers/theme_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -19,18 +21,28 @@ class SettingsScreen extends StatelessWidget {
   }
 }
 
-class _SettingView extends StatelessWidget {
+class _SettingView extends ConsumerWidget {
   const _SettingView();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+    final isColor = Theme.of(context).colorScheme;
+    final isDarkMode = ref.watch(themeNotifierProvider).isDarkMode;
     return Column(
       children: [
         SwitchListTile(
-          title: Text('Modo desarrollador'),
-          subtitle: Text('Controles extras'),
-          value: true,
-          onChanged: (value) {},
+          title: Text('Imagen de fondo'),
+          subtitle: isDarkMode
+              ? Text('Modo Noche Activado')
+              : Text('Modo Día Activado'),
+          value: isDarkMode,
+          onChanged: (isDarkMode) {
+            ref.watch(themeNotifierProvider.notifier).toggleDarkMode();
+          },
+          secondary: Icon(
+            isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
+            color: isColor.primary,
+          ),
         ),
         Expanded(
           child: _GenerarSettingItems(),
